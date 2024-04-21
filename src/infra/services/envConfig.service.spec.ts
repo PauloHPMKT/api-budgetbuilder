15b554a59c631481d2e938dafb9a6d23fab2ef10
Paulo Sergio <pauloserg861@gmail.com>
2024-04-21 20:47:32 -0300

    test: ensure port receive a number

--- src/infra/services/envConfig.service.spec.ts
+++ src/infra/services/envConfig.service.spec.ts
@@ -6,9 +6,18 @@ const makeSut = () => {
 }
 
 describe("EnvConfigService", () => {
+  const { sut } = makeSut();
+  sut.envPathConstruction('test')
+
   it("should instantiate EnvConfigService", () => {
     const { sut } = makeSut();
     expect(sut).toBeTruthy();
     expect(sut).toBeDefined();
+  });
+
+  it("should return a number", () => {
+    const port = sut.getAppPort();
+    expect(typeof port).toBe('number');
+    expect(port).toBe(3006);
   });
 })
