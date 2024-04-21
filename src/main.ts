93ff3083a78371e43f795d08006106ae2de76324
Paulo Sergio <pauloserg861@gmail.com>
2024-04-21 13:05:03 -0300

    chore: condig init server

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { App } from "./presentation/App";
+
+const port = 3006;
+
+const app = new App();
+app.listen(port);
---
+++ src/presentation/App.ts
@@ -0,0 +1,16 @@
+import express, { Application } from 'express';
+
+export class App {
+    public readonly app: Application;
+
+    constructor() {
+        this.app = express();
+    }
+
+    listen(port: number) {
+        const host = `http://localhost:${port}`;
+        this.app.listen(port, () => {
+            console.log(`[API_BUDGETBUILDER] - Server is running on host: ${host}`);
+        });
+    }
+}
